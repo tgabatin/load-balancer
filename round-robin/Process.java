4ad3ad0f456db06c651bd05ff4fb8b3253e4226f
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-25 11:10:07 -0600

    Added the functions for the Process.java class

---
+++ round-robin/Process.java
@@ -0,0 +1,21 @@
+public class Process {
+    private int id;
+    private int burstTime;
+
+    public Process(int id, int burstTime) {
+        this.id = id;
+        this.burstTime = burstTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+}
