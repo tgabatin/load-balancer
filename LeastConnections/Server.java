abe965fe218fa686eae697413c85c5aab075666b
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-30 19:56:51 -0600

    Added the functions for the Server class for the Least Connections

--- LeastConnections/Server.java
+++ LeastConnections/Server.java
@@ -1,5 +1,22 @@
 package LeastConnections;
 
 public class Server {
-    
+    private int connections;
+
+    public Server() {
+        this.connections = 0;
+    }
+
+    public int getConnections() {
+        return connections;
+    }
+
+    public void incrementConnections() {
+        ++connections;
+    }
+
+    public void decrementConnections() {
+        --connections;
+    }
+
 }
