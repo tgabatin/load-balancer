9b6e173a977bd4539ef33b365dd9114ed73afc34
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-05-01 15:40:44 -0600

    Finished the LoadBalancer class

--- LeastConnections/LoadBalancer.java
+++ LeastConnections/LoadBalancer.java
@@ -1,5 +1,30 @@
 package LeastConnections;
 
+import java.util.Arrays;
+
 public class LoadBalancer {
-    
+    private Server[] servers;
+
+    public LoadBalancer(int numServers) {
+        servers = new Server[numServers];
+        for (int i = 0; i < numServers; i++) {
+            servers[i] = new Server();
+        }
+    }
+
+    private Server findServerWithFewestConnections() {
+        Server selectedServer = servers[0];
+        for (int i = 1; i < servers.length; i++) {
+            if (servers[i].getConnections() < selectedServer.getConnections()) {
+                selectedServer = servers[i];
+            }
+        }
+        return selectedServer;
+    }
+
+    public void distribute() {
+        Server selectedServer = findServerWithFewestConnections();
+        selectedServer.incrementConnections();
+        System.out.println("Traffic directed to server with " + selectedServer.getConnections() + " connections");
+    }
 }
