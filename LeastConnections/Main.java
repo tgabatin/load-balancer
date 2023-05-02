2cb557e3b8f787b794e7888f1c89addc795eb676
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-05-02 12:49:56 -0600

    Added the main function

--- LeastConnections/Main.java
+++ LeastConnections/Main.java
@@ -1,5 +1,10 @@
 package LeastConnections;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        LoadBalancer lb = new LoadBalancer(3);
+        for (int i = 0; i < 10; i++) {
+            lb.distribute();
+        }
+    }
 }
