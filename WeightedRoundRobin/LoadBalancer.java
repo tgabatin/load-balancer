38b8b48ecb657fe44e3964ccdff351935451aef1
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-05-03 22:15:47 -0600

    Added the new class for the Load Balancer

--- WeightedRoundRobin/LoadBalancer.java
+++ WeightedRoundRobin/LoadBalancer.java
@@ -1,5 +1,17 @@
 package WeightedRoundRobin;
 
+import java.util.List;
+
 public class LoadBalancer {
+    private List<Server> servers;
+    private int gcd;
+    private int maxWeight;
+    private int[] nextServer;
+    private int[] currentWeight;
+    private int currentIndex;
+
+    public LoadBalancer(List<Server> servers) {
+        
+    }
     
 }
--- WeightedRoundRobin/Server.java
+++ WeightedRoundRobin/Server.java
@@ -1,5 +1,32 @@
 package WeightedRoundRobin;
 
 public class Server {
-    
+    private int id;
+    private int weight;
+
+    /**
+     * 
+     * @param id
+     * @param weight
+     */
+    public Server(int id, int weight) {
+        this.id = id;
+        this.weight = weight;
+    }
+
+    /**
+     * 
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * @return weight
+     */
+    public int getWeight() {
+        return weight;
+    }
 }
