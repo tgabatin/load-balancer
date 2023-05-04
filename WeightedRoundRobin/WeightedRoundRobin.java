c00c806351f4ef7a0fbfacaeb6d1c206612377f9
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-05-04 13:57:07 -0600

    Added the WeightedRoundRobin main - need to fix

--- WeightedRoundRobin/WeightedRoundRobin.java
+++ WeightedRoundRobin/WeightedRoundRobin.java
@@ -1,5 +1,25 @@
 package WeightedRoundRobin;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class WeightedRoundRobin {
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter number of servers: ");
+    int n = scanner.nextInt();
+    List<Server> servers = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+        System.out.print("Enter weight for server " + (i+1) + ": ");
+        int weight = scanner.nextInt();
+        servers.add(new Server(i, weight));
+    }
+    System.out.print("Enter number of requests: ");
+    int m = scanner.nextInt();
+    LoadBalancer lb = new LoadBalancer(servers);
+    for (int j = 0; i < m; j++) {
+        int serverId = lb.assignRequest();
+        System.out.println("Request " + (j+1) + " assigned to server " + (serverId+1));
+    }
 }
+
